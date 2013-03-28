64b11d1ea6646ad04ebeff482ea9269c0d1006b2
Dave Grijalva <grijalva@gmail.com>
2013-03-28 10:51:00 -0700

    convenience method

---
+++ upstream/utilities.go
@@ -0,0 +1,24 @@
+package upstream
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/fitstar/falcore"
+)
+
+// fixme: probably should use net.SplitHostPort
+func SplitHostPort(hostPort string, defaultPort int)(string, int){
+	parts := strings.Split(hostPort, ":")
+	upstreamHost := parts[0]
+	upstreamPort := defaultPort
+	if len(parts) > 1 {
+		var err error
+		upstreamPort, err = strconv.Atoi(parts[1])
+		if err != nil {
+			upstreamPort = defaultPort
+			falcore.Error("UpstreamPool Error converting port to int for", upstreamHost, ":", err)
+		}
+	}
+	return upstreamHost, upstreamPort
+}
