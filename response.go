47c8de3629c23e595351e39d903fd5355bb87d62
Dave Grijalva <grijalva@gmail.com>
2013-04-03 14:59:52 -0700

    documenting response generators

--- response.go
+++ response.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 )
 
+// Generate an http.Response using the basic fields
 func SimpleResponse(req *http.Request, status int, headers http.Header, contentLength int64, body io.Reader) *http.Response {
 	res := new(http.Response)
 	res.StatusCode = status
@@ -28,20 +29,26 @@ func SimpleResponse(req *http.Request, status int, headers http.Header, contentL
 	return res
 }
 
+// Like SimpleResponse but uses a []byte for the body.
 func ByteResponse(req *http.Request, status int, headers http.Header, body []byte) *http.Response {
 	return SimpleResponse(req, status, headers, int64(len(body)), bytes.NewBuffer(body))
 }
 
+// Like StringResponse but uses a string for the body.
 func StringResponse(req *http.Request, status int, headers http.Header, body string) *http.Response {
 	return SimpleResponse(req, status, headers, int64(len(body)), strings.NewReader(body))
 }
 
+// A 302 redirect response
 func RedirectResponse(req *http.Request, url string) *http.Response {
 	h := make(http.Header)
 	h.Set("Location", url)
 	return SimpleResponse(req, 302, h, 0, nil)
 }
 
+// Generate an http.Response by json encoding body using
+// the standard library's json.Encoder.  error will be nil
+// unless json encoding fails.
 func JSONResponse(req *http.Request, status int, headers http.Header, body interface{}) (*http.Response, error) {
 	buf := new(bytes.Buffer)
 	if err := json.NewEncoder(buf).Encode(body); err != nil {
