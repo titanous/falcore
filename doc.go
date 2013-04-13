9c0ca655aed08aad411908099a57f7a39a8c1c26
Dave Grijalva <grijalva@gmail.com>
2013-04-13 13:47:19 -0700

    Update doc.go with pointers to new project page

--- doc.go
+++ doc.go
@@ -1,3 +1,4 @@
 // Package falcore is a framework for constructing high performance, modular HTTP servers.
-// For more information, see README.md
+// For more information, see the project page at http://fitstar.github.io/falcore.
+// Or read the full package documentation at http://godoc.org/github.com/fitstar/falcore
 package falcore
