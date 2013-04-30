6690c179b7bf2b9114a26b386fc03b6ccf0ed5bd
Dave Grijalva <grijalva@gmail.com>
2013-04-30 10:48:39 -0700

    test 1.0 and 1.1 keepalive cases.  includes failing test fixed by PR#14

--- keep_alive_test.go
+++ keep_alive_test.go
@@ -8,6 +8,18 @@ import (
 	"bufio"
 )
 
+var keepAliveTestData = []struct{
+	name string
+	version int
+	useHeader bool
+	shouldKeepAlive bool
+}{
+	{ "1.0", 0, true, true },
+	{ "1.0 no KA", 0, false, false },
+	{ "1.1", 1, true, true },
+	{ "1.1 no KA", 1, false, true },
+}
+
 func TestKeepAlive(t *testing.T){
 	// Startup a basic server and get the port
 	pipeline := NewPipeline()
@@ -20,24 +32,40 @@ func TestKeepAlive(t *testing.T){
 	
 	// Connect and make some requests
 	// Not using http.Client because transport does too many magics
-	conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%v", serverPort))
-	bconn := bufio.NewReader(conn)
-	if err != nil {
-		t.Fatal("Couldn't connect")
-	}
-	for i := 0; i < 5; i++ {
-		req, _ := http.NewRequest("GET", "/", nil)
-		req.Header.Set("Connection", "Keep-Alive")
-		if err = req.Write(conn); err != nil {
-			t.Fatal(fmt.Sprintf("[%v] Couldn't write request: %v", i, err))
-		}
-		res, err := http.ReadResponse(bconn, req)
+	for _, test := range keepAliveTestData {
+		conn, err := net.Dial("tcp", fmt.Sprintf("localhost:%v", serverPort))
+		bconn := bufio.NewReader(conn)
 		if err != nil {
-			t.Fatal(fmt.Sprintf("[%v] Couldn't read response: %v", i, err))
+			t.Fatal("Couldn't connect")
 		}
-		res.Body.Close()
+		for i := 0; i < 5; i++ {
+			req, _ := http.NewRequest("GET", "/", nil)
+			req.ProtoMinor = test.version
+			req.Proto = fmt.Sprintf("HTTP/1.%v", test.version)
+			if test.useHeader {
+				req.Header.Set("Connection", "Keep-Alive")
+			}
+			if err = req.Write(conn); err != nil {
+				if test.shouldKeepAlive {
+					t.Error(fmt.Sprintf("[%v:%v] Couldn't write request: %v", test.name, i, err))
+				}
+				break
+			}
+			
+			res, err := http.ReadResponse(bconn, req)
+			if err != nil {
+				if test.shouldKeepAlive {
+					t.Error(fmt.Sprintf("[%v:%v] Couldn't read response: %v", test.name, i, err))
+				}
+				break
+			} else if !test.shouldKeepAlive && i > 0 {
+				t.Error(fmt.Sprintf("[%v:%v] Connection should be closed", test.name, i))
+				break
+			}
+			res.Body.Close()
+		}
+		conn.Close()
 	}
-	
 	
 	// Clean up
 	srv.StopAccepting()
