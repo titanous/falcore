f43a7e379c483160f32a4675a12668f3e299f6b6
Dave Grijalva <grijalva@gmail.com>
2013-06-03 18:23:02 -0700

    fixes issues with throttling

--- filter/throttle.go
+++ filter/throttle.go
@@ -3,7 +3,7 @@ package filter
 import (
 	"github.com/fitstar/falcore"
 	"net/http"
-	"regexp"
+	"sync"
 	"sync/atomic"
 	"time"
 )
@@ -14,8 +14,9 @@ type Throttler struct {
 	Condition func(req *falcore.Request) bool // If this is set, and returns false, the request will not be throttled
 	count     int64
 
-	ticker  *time.Ticker
-	tickerM *sync.RWMutex
+	ticker      *time.Ticker
+	tickerM     *sync.RWMutex
+	tickerClose chan bool
 }
 
 // Type check
@@ -25,9 +26,7 @@ func NewThrottler(RPS int) *Throttler {
 	th := new(Throttler)
 	atomic.StoreInt64(&th.count, 0)
 	th.tickerM = new(sync.RWMutex)
-	if RPS > 0 {
-		th.ticker = time.NewTicker(time.Second / time.Duration(RPS))
-	}
+	th.SetRPS(RPS)
 	return th
 }
 
@@ -45,22 +44,21 @@ func (t *Throttler) FilterRequest(req *falcore.Request) *http.Response {
 	if tt != nil {
 		req.CurrentStage.Status = 1
 		atomic.AddInt64(&t.count, 1)
+	TICK:
 		for {
-			// If the req completes because the channel was closed,
-			// grab the new ticker and try again.
-			_, ok := <-tt.C
-			if ok {
-				break
-			}
+			select {
+			case <-tt.C:
+				break TICK
+			case <-t.tickerClose:
+				// Get new ticker
+				t.tickerM.RLock()
+				tt = t.ticker
+				t.tickerM.RUnlock()
 
-			// Get new ticker
-			t.tickerM.RLock()
-			tt = t.ticker
-			t.tickerM.RUnlock()
-
-			// If throttling has been disabled, continue.
-			if t.ticker == nil {
-				break
+				// If throttling has been disabled, continue.
+				if t.ticker == nil {
+					break TICK
+				}
 			}
 		}
 		atomic.AddInt64(&t.count, -1)
@@ -73,14 +71,24 @@ func (t *Throttler) SetRPS(RPS int) {
 	t.tickerM.Lock()
 	defer t.tickerM.Unlock()
 
-	oldTicker := t.ticker
+	// Stop the old ticker
+	if t.ticker != nil {
+		t.ticker.Stop()
+	}
+
+	// Creat the new ticker
 	if RPS > 0 {
 		t.ticker = time.NewTicker(time.Second / time.Duration(RPS))
 	} else {
 		t.ticker = nil
 	}
-	oldTicker.Stop()   // doesn't close C
-	close(oldTicker.C) // signal to waiting requests they should look for a new ticker
+
+	// signal to waiting requests they should look for a new ticker
+	oldTickerC := t.tickerClose
+	t.tickerClose = make(chan bool)
+	if oldTickerC != nil {
+		close(oldTickerC)
+	}
 }
 
 // Returns the number of requests waiting on the throttler
