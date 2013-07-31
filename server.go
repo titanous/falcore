1e1ced003c0611ff73676722ba05ad151205d8e2
Jonathan Rudenberg <jonathan@titanous.com>
2013-07-31 19:34:25 -0400

    Don't crash the server if a pipeline panics and there is a handler

--- server.go
+++ server.go
@@ -30,7 +30,8 @@ type Server struct {
 	logPrefix          string
 	AcceptReady        chan int
 	bufferPool         *BufferPool
-	writeBufferPool *WriteBufferPool
+	writeBufferPool    *WriteBufferPool
+	PanicHandler       func(conn net.Conn, err interface{})
 }
 
 type RequestCompletionCallback func(req *Request, res *http.Response)
@@ -362,6 +363,11 @@ func (srv *Server) requestFinished(request *Request, res *http.Response) {
 }
 
 func (srv *Server) connectionFinished(c net.Conn, closeChan chan int) {
+	if srv.PanicHandler != nil {
+		if err := recover(); err != nil {
+			srv.PanicHandler(c, err)
+		}
+	}
 	c.Close()
 	close(closeChan)
 	srv.handlerWaitGroup.Done()
